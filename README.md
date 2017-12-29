d204b25c56dbe3fa4203db69e0ebcbea281a40f5
Justin Bender <jrkbender@gmail.com>
2017-12-29 18:32:55 -0500

    finished FizzBuzz algorithm

---
+++ README.md
@@ -0,0 +1,13 @@
+Using the modulus operator to make the variable change to fizz, buzz or, fizz buzz.
+
+fizz buzz being divisible by 3 + 5
+fizz being divisible by 3
+buzz being divisible by 5
+
+resulting in a console.log of
+1
+2
+Fizz
+4
+buzz
+etc......
--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello');
+var fizzBuzz = function(num){
+  for(var i = 1; i <= num; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+      console.log("Fizz Buzz");
+    } else if(i % 3 === 0) {
+      console.log("Fizz");
+    } else if(i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+
+  console.log('Function  was fizzBuzz('+ num + ')');
+}
+fizzBuzz(40);
